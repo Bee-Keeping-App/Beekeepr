256b3c78f4876cad0e1ccf5412dbbac8e7c390b8
jaocbhebbel <jacob.hebbel@gmail.com>
2025-11-27 12:15:02 -0500

    made a readme for the backend

--- backend/controllers/accounts.controller.js
+++ backend/controllers/accounts.controller.js
@@ -1,21 +1,24 @@
 const service = require("../services/accounts.service");
 
-
+/* Read all caller */
 exports.getAll = async (req, res) => {
     const accounts = await service.findAll();
     res.json(accounts);
 };
 
+/* Read one caller */
 exports.getOne = async (req, res) => {
     const account = await service.findOne(req.params.id);
     res.json(account);
 };
 
+/* Insert caller */
 exports.insert = async (req, res) => {
     const account = await service.insertOne(req.body);
     res.status(201).json(account);
 };
 
+/* Update caller */
 exports.update = async (req, res) => {
     const account = await accountService.updateOne(
         req.params.id,
@@ -24,6 +27,7 @@ exports.update = async (req, res) => {
   res.json(account);
 }
 
+/* Delete caller */
 exports.delete = async (req, res) => {
     await accountService.remove(req.params.id);
     res.status(204).send();
