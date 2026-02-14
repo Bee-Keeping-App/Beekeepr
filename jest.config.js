702cef954a8adbb3a1067879eef30a63a9e833bc
jaocbhebbel <jacob.hebbel@gmail.com>
2026-02-13 20:17:15 -0500

    updated package and jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
-module.exports = {
+export default {
     testEnvironment: 'node',
     setupFiles: ['<rootDir>/tests/setupEnv.js'],
     setupFilesAfterEnv: ['<rootDir>/tests/setupTests.js'],
+    transform: {},
     verbose: true,
     testTimeout: 20000
 };
